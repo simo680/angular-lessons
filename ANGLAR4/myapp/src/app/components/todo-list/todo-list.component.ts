c0ccab0ca1279d70dfbbbd3361ff4640b43dc7a9
simo680 <erik.simonyan2005@gmail.com>
2024-04-02 16:09:01 +0300

    update commit

--- ANGLAR4/myapp/src/app/components/todo-list/todo-list.component.ts
+++ ANGLAR4/myapp/src/app/components/todo-list/todo-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo, TodoService } from '../../services/todo.service';
-import { Observable } from 'rxjs';
+import { Observable, filter, map } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-
 
 @Component({
   selector: 'app-todo-list',
@@ -15,7 +14,33 @@ export class TodoListComponent implements OnInit {
   public todos$: Observable<Todo[]>;
   constructor(private todoService: TodoService) {}
 
+  public observable = new Observable((observer) => {
+    observer.next('Value 1');
+    observer.next('Value 2');
+    observer.next('Value 3');
+    observer.next('Value 4');
+    observer.complete();
+  });
+
   ngOnInit(): void {
     this.todos$ = this.todoService.getTodos();
+    // this.todos$.subscribe((x) => console.log(1, x))
+
+    this.observable
+      .pipe(
+        map((value) => value + 'Test'),
+        filter((value) => value.includes('2') || value.includes('4'))
+      )
+      .subscribe({
+        next: (value) => console.log(value),
+        error: (error) => console.log(error),
+        complete: () => console.log('done'),
+      });
+
+    // this.observable.subscribe({
+    //   next: (value) => console.log(value),
+    //   error: (error) => console.log(error),
+    //   complete: () => console.log('done'),
+    // });
   }
 }
